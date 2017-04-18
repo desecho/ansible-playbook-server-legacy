15f7208696c6f5d0f36749398c511e49936a08a3
Anton Samarchyan <desecho@gmail.com>
2017-04-18 00:16:54 -0400

    changes

--- ansible/roles/common/templates/deploy.sh
+++ ansible/roles/common/templates/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $1
+cd {{ home }}/$1
 git pull
 bower install --allow-root
 source /opt/$1/env/bin/activate
@@ -8,6 +8,7 @@ pip install -r requirements.txt
 cd /opt/$1/project/src
 ./manage.py migrate
 ./manage.py collectstatic --noinput
+
 {% if is_prod -%}
 systemctl restart $1-wsgi
 service nginx restart
