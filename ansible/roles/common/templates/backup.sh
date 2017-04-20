f6b2a514e5a302c4863bfcf4b009a82a02194658
Anton Samarchyan <desecho@gmail.com>
2017-04-20 02:35:38 -0400

    changes

--- ansible/roles/common/templates/backup.sh
+++ ansible/roles/common/templates/backup.sh
@@ -3,3 +3,5 @@
 {{ scripts_path }}/mysql_backup.sh
 {{ scripts_path }}/google_drive_backup.sh
 {{ scripts_path }}/reboot_if_needed.sh
+
+curl --retry 3 https://hchk.io/0ef8e8d4-c51f-4746-a887-70c2fe891648
--- ansible/roles/common/templates/mysql_backup.sh
+++ ansible/roles/common/templates/mysql_backup.sh
@@ -12,7 +12,8 @@ MYSQL="$(which mysql)"
 GZIP="$(which gzip)"
 NOW=$(date +"%d-%m-%Y")
 
-DBS="$($MYSQL -u $MYSQL_USER -h $MYSQL_HOST -p$MYSQL_PASSWORD -Bse 'show databases')"
+# DBS="$($MYSQL -u $MYSQL_USER -h $MYSQL_HOST -p$MYSQL_PASSWORD -Bse 'show databases')"
+DBS="$($MYSQL -u $MYSQL_USER -h $MYSQL_HOST -Bse 'show databases')"
 DBS=${DBS//information_schema/}
 DBS=${DBS//mysql/}
 DBS=${DBS//performance_schema/}
@@ -22,5 +23,6 @@ mkdir -p $DIR/$NOW
 for db in $DBS
 do
  FILE=$DIR/$NOW/$db.$NOW.sql.gz
- $MYSQL_DUMP -u $MYSQL_USER -h $MYSQL_HOST -p$MYSQL_PASSWORD $db | $GZIP -9 > $FILE
+ $MYSQL_DUMP -u $MYSQL_USER -h $MYSQL_HOST $db | $GZIP -9 > $FILE
+ # $MYSQL_DUMP -u $MYSQL_USER -h $MYSQL_HOST -p$MYSQL_PASSWORD $db | $GZIP -9 > $FILE
 done
