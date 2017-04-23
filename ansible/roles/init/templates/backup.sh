2743f6d6c4e60c4aa36cbf532f7c8bed5ee563fa
Anton Samarchyan <desecho@gmail.com>
2017-04-23 10:59:09 -0400

    Changes

--- ansible/roles/init/templates/backup.sh
+++ ansible/roles/init/templates/backup.sh
@@ -3,5 +3,3 @@
 {{ scripts_path }}/mysql_backup.sh
 {{ scripts_path }}/google_drive_backup.sh
 {{ scripts_path }}/reboot_if_needed.sh
-
-curl --retry 3 {{ track_code_backup }}
--- ansible/roles/init/templates/backup_cron.sh
+++ ansible/roles/init/templates/backup_cron.sh
@@ -4,3 +4,5 @@ info() { while IFS='' read -r line; do echo "$(date) - system - backup - $line" 
 error() { while IFS='' read -r line; do echo "$(date) - system - backup - $line" >> {{ logs_path }}/errors.log; done; };
 
 {{ scripts_path }}/backup.sh 1> >(info) 2> >(error)
+
+curl --retry 3 https://hchk.io/{{ track_code_backup }}
