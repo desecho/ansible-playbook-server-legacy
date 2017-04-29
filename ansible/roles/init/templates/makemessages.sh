65d7ca937514a3700c3218b2367720e539010e66
Anton Samarchyan <desecho@gmail.com>
2017-04-29 00:36:52 -0400

    Changes

--- ansible/roles/init/templates/makemessages.sh
+++ ansible/roles/init/templates/makemessages.sh
@@ -8,4 +8,9 @@ fi
 source /opt/$1/env/bin/activate
 cd /opt/$1/project/src
 ./manage.py makemessages
-./manage.py makemessages -d djangojs
+
+if [ $1 = "movies" ]; then
+   ./manage.py makemessages -d djangojs --ignore=moviesapp/static/bower/*
+else
+  ./manage.py makemessages -d djangojs
+fi
