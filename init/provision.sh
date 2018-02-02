39d0a5e025ebd75a600617e5dfd09a734e30ae5d
Anton Samarchyan <desecho@gmail.com>
2018-02-02 02:25:05 -0500

    Changes

--- init/provision.sh
+++ init/provision.sh
@@ -3,4 +3,4 @@
 apt-get install python-pip -y
 # It's required to also install these packages for ansible installation (cryptography dependencies)
 sudo apt-get install libffi-dev g++ libssl-dev -y
-pip install ansible==2.3.0.0
+pip install ansible==2.3.2
