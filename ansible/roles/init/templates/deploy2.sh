4f66ec94c4b46f7cca81f71e7bba6d00820525f8
Anton Samarchyan <desecho@gmail.com>
2018-02-03 13:31:29 -0500

    Changes

---
+++ ansible/roles/init/templates/deploy2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+if [[ $# -eq 0 ]] ; then
+    echo "Enter project name"
+    exit 0
+fi
+
+cd {{ home }}/$1
+git pull
+bower install --allow-root
+source /opt/$1/env/bin/activate
+pip install -r requirements.txt --exists-action s
+cd /opt/$1/project
+./manage.py migrate
+./manage.py collectstatic --noinput
+
+{% if is_prod -%}
+systemctl restart $1-wsgi
+service nginx restart
+{%- endif %}
