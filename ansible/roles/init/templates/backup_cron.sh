5e14eaac63b73b871b7b165b0c1b8ad6d577a2bd
Anton Samarchyan <desecho@gmail.com>
2018-02-12 00:47:42 -0500

    Changes

--- ansible/roles/init/templates/backup_cron.sh
+++ ansible/roles/init/templates/backup_cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 info() { while IFS='' read -r line; do echo "$(date) - system - backup - $line" >> {{ logs_path }}/errors.log; done; };
-error() { while IFS='' read -r line; do echo "$(date) - system - backup - $line" >> {{ logs_path }}/errors.log; done; };
+error() { while IFS='' read -r line; do echo "$(date) - system - backup - $line" >> {{ logs_path }}/info.log; done; };
 
 {{ scripts_path }}/backup.sh 1> >(info) 2> >(error)
 
