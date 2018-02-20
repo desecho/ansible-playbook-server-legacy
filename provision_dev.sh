72abfccd0dfce9fbbf7409fdaa67e7264d8aa3e5
Anton Samarchyan <desecho@gmail.com>
2018-02-20 03:48:20 -0500

    Changes

--- provision_dev.sh
+++ provision_dev.sh
@@ -2,22 +2,8 @@
 
 set -eu
 
-if [ $1 = "init" ]; then
-	if [ $DESKTOP = 1 ]; then
-   		./provision.sh $1 desktop
-    else
-    	./provision.sh $1 dev
-    fi
-    exit 0
+if [ $DESKTOP = 1 ]; then
+    ./provision.sh $1 desktop
+else
+    ./provision.sh $1 dev
 fi
-
-if [ $1 = "blog" ]; then
-    if [ $DESKTOP = 1 ]; then
-        ./provision.sh $1 desktop
-    else
-        ./provision.sh $1 dev
-    fi
-    exit 0
-fi
-
-./provision.sh $1 dev
