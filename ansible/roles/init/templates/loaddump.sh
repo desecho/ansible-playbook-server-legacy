2b09325e596a3c6296035748be976fd0c18b10d6
Anton Samarchyan <desecho@gmail.com>
2018-03-24 05:38:54 -0400

    Changes

--- ansible/roles/init/templates/loaddump.sh
+++ ansible/roles/init/templates/loaddump.sh
@@ -6,5 +6,12 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 yesterday=`date -d "yesterday" '+%d-%m-%Y'`
+
+{% if is_dev -%}
 scp "prod:/var/backups/$yesterday/$1.$yesterday.sql.gz" /tmp/dump.sql.gz
 gunzip -c /tmp/dump.sql.gz | /opt/$1/restoredb.sh
+{%- endif %}
+
+{% if is_prod -%}
+gunzip -c /var/backups/$yesterday/$1.$yesterday.sql.gz | /opt/$1/restoredb.sh
+{%- endif %}
