76c1d44f3206ec4d7ce7a16bb6b6abbee58df97d
Anton Samarchyan <desecho@gmail.com>
2019-05-24 06:02:40 -0400

    Changes

--- init/clone_dev.sh
+++ init/clone_dev.sh
@@ -4,5 +4,8 @@ cd ~
 git clone git@github.com:desecho/movies.git
 git clone git@github.com:desecho/ghcontrib.git
 git clone git@github.com:desecho/blog.git
+cd blog
+git submodule update
+cd ..
 git clone git@github.com:desecho/desecho.org.git
 #git clone git@github.com:desecho/ighelper.git
